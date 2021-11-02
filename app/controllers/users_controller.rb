3bdde2c3f59ec006ad0a153d768a3bcb82dd5beb
Jason Moritz <moritz.jason@gmail.com>
2021-11-02 11:29:44 -0500

    defined users controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,28 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: :update
 
   # GET /users
-  def index
-    @users = User.all
+  # def index
+  #   @users = User.all
 
-    render json: @users
-  end
+  #   render json: @users
+  # end
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+  # def show
+  #   render json: @user
+  # end
 
   # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode({ id: @user.id })
+      render json: {
+        user: @user.attributes.except('password_digest'), 
+        token: @token
+      }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -27,16 +31,20 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      @token = encode({ id: @user.id })
+      render json: {
+        user: @user.attributes.except('password_digest'),
+        token: @token
+      }, status: :accepted 
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -46,6 +54,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email, :image_url, :password_digest)
+      params.require(:user).permit(:username, :email, :image_url, :password)
     end
 end
