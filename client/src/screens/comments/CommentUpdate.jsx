ea9d27569d56eb0d0f251c13e367731e9794fbf6
Jason Moritz <moritz.jason@gmail.com>
2021-11-04 15:10:13 -0500

    working on prefilled form for comment update

--- client/src/screens/comments/CommentUpdate.jsx
+++ client/src/screens/comments/CommentUpdate.jsx
@@ -1,4 +1,36 @@
-export default function CommentUpdate() {
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { TextField } from '@mui/material'
+
+
+export default function CommentUpdate({ handleCommentUpdate }) {
+    const [formData, setFormData] = useState({
+        content: ''
+    })
+
+    const { content } = formData
+    const { id } = useParams()
+
+    useEffect(() => {
+        const prefillFormData = () => {
+
+        }
+        
+    },[id])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        handleCommentUpdate(formData)
+    }
+
     return (
         <div>
             
