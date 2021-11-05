9e677a0916180057ffeb3d95892bdf2cb67a85ca
Jason Moritz <moritz.jason@gmail.com>
2021-11-05 15:18:35 -0500

    Started CommentCard.jsx styling

--- client/src/components/CommentCard.jsx
+++ client/src/components/CommentCard.jsx
@@ -1,22 +1,42 @@
 import './CommentCard.css'
 import { Link, useParams } from 'react-router-dom'
+import { Button } from '@mui/material'
 
+export default function CommentCard({
+  currentUser,
+  comment,
+  handleCommentDelete,
+}) {
+  const { id } = useParams()
 
-export default function CommentCard({ currentUser, comment, handleCommentDelete }) {
-    const { id } = useParams()
-
-    return (
-        <div className='comment-card'>
-            <img src={comment?.user.image_url} alt='profile-pic' />
-            <h3>{comment?.user.username}</h3>
-            <h5>{comment?.content}</h5>
-            {currentUser && currentUser.id === comment?.user_id ? (
-                <>
-                    <Link to={`/posts/${comment.post_id}/comments/${comment.id}/update`}>Edit</Link>
-                    <button onClick={() => handleCommentDelete(comment.post_id, comment.id)}>Delete</button>
-                </>
-            ) : (null)
-            }
-        </div>
-    )
+  return (
+    <div className='comment-card'>
+      <div className='comment-card-user'>
+        <img
+          className='avatar'
+          src={comment?.user.image_url}
+          alt='profile-pic'
+        />
+        <h3 className='username'>{comment?.user.username}</h3>
+      </div>
+      <div className='comment-card-content'>
+        <h5>{comment?.content}</h5>
+      </div>
+      {currentUser && currentUser.id === comment?.user_id ? (
+        <>
+          <Link
+            className='comment-card-link'
+            to={`/posts/${comment.post_id}/comments/${comment.id}/update`}
+          >
+            <Button>Edit</Button>
+          </Link>
+          <Button
+            onClick={() => handleCommentDelete(comment.post_id, comment.id)}
+          >
+            Delete
+          </Button>
+        </>
+      ) : null}
+    </div>
+  )
 }
