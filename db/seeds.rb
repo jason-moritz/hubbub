c0d1d67b6e4403eecb5a48f9287db60dca6a245d
Jason Moritz <moritz.jason@gmail.com>
2021-11-06 15:28:19 -0500

    updated seed data to drop all comments from db upon run and added new migration info in seed creates. Also adjusted post title faker data to 2 sentences to prevent length validation from erroring out.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
-@admin = User.create!(username: 'admin', email: 'test@test.com', password: '123456', image_url: 'https://res.cloudinary.com/dwhdm5z4a/image/upload/v1636123204/hubbub_images/wqfo905juwsykmwbfdi2.jpg')
-@admin2 = User.create!(username: 'hummingbirds', email: 'test2@test.com', password: '123456', image_url: 'https://res.cloudinary.com/dwhdm5z4a/image/upload/v1636070712/hubbub_images/xesamqfajtcsvpvn24av.jpg')
+@admin = User.create!(username: 'admin', email: 'test@test.com', password: '123456',
+                      image_url: 'https://res.cloudinary.com/dwhdm5z4a/image/upload/v1636123204/hubbub_images/wqfo905juwsykmwbfdi2.jpg', public_img: 'hubbub_images/wqfo905juwsykmwbfdi2')
+@admin2 = User.create!(username: 'hummingbirds', email: 'test2@test.com', password: '123456',
+                       image_url: 'https://res.cloudinary.com/dwhdm5z4a/image/upload/v1636070712/hubbub_images/xesamqfajtcsvpvn24av.jpg', public_img: 'hubbub_images/xesamqfajtcsvpvn24av')
 puts "#{User.count} users created!"
 
 10.times do
-  Post.create!(title: Faker::Lorem.words(number: 3), content: Faker::Lorem.paragraph(sentence_count: 4),
-               image_url: Faker::Internet.url, user_id: @admin.id)
+  Post.create!(title: Faker::Lorem.words(number: 2), content: Faker::Lorem.paragraph(sentence_count: 4),
+               image_url: Faker::Internet.url, public_img: Faker::Internet.url, user_id: @admin.id)
 end
 
 puts "#{Post.count} posts created!"
