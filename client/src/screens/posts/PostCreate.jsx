d114a3143169376f6fc22f4e4a6bed88d5b91caf
Jason Moritz <moritz.jason@gmail.com>
2021-11-06 11:35:00 -0500

    added column to posts schema to include public photo url because it is needed to remove and update photos with cloudinary

--- client/src/screens/posts/PostCreate.jsx
+++ client/src/screens/posts/PostCreate.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { Button, TextField } from '@mui/material'
 
-export default function PostCreate({ handlePostCreate }) {
+export default function PostCreate({ handlePostCreate, handleImageUpload }) {
   const [formData, setFormData] = useState({
     title: '',
     content: '',
     image_url: '',
+    public_img: '',
   })
   const { title, content, image_url } = formData
 
@@ -22,31 +23,61 @@ export default function PostCreate({ handlePostCreate }) {
     handlePostCreate(formData)
   }
 
+  const handleImage = async e => {
+    const uploaded_url = await handleImageUpload(e.target.files[0])
+    setFormData(prevState => ({
+      ...prevState,
+      image_url: uploaded_url.secure_url,
+      public_img: uploaded_url.public_id,
+    }))
+  }
+
   return (
     <div>
       <h1>Create a new post!</h1>
-      <form onSubmit={handleSubmit}>
+      <form className='post-create-form' onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          id='post-create-image'
+          type='file'
+          label='Image'
+          margin='normal'
+          color={formData.image_url ? 'success' : 'primary'}
+          helperText={
+            formData.image_url
+              ? 'Image uploaded successfully!'
+              : 'Upload a photo'
+          }
+          onChange={handleChange}
+        />
         <TextField
           autoFocus
+          required
+          fullWidth
+          id='post-create-title'
           type='text'
+          variant='standard'
           label='Title'
           name='title'
+          margin='normal'
           value={title}
           onChange={handleChange}
+          inputProps={{ minLength: 3, maxLength: 30 }}
         />
         <TextField
+          required
+          fullWidth
+          id='post-create-content'
           type='text'
+          variant='standard'
           label='Content'
           name='content'
+          margin='normal'
+          multiline={true}
+          rows={5}
           value={content}
           onChange={handleChange}
-        />
-        <TextField
-          type='text'
-          label='Image'
-          name='image_url'
-          value={image_url}
-          onChange={handleChange}
+          inputProps={{ minLength: 3, maxLength: 240 }}
         />
         <Button type='submit'>Submit</Button>
       </form>
